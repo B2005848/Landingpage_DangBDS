fd20369eaa0e981736e2d83dab278fb9325b01e1
B2005848 <namltgt2002@gmail.com>
2025-08-16 21:59:04 +0700

    using animate

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,9 +22,18 @@ export default function RootLayout({
     <html lang="vi">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+        />
       </head>
+
       <body className={`${montserrats.className} pt-16`}>
         <Header />
+        <div className="animate__animated animate__tada animate__infinite infinite">
+          Example
+        </div>
         {children}
         {/* <Footer /> */}
       </body>
